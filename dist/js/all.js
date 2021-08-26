f5197fce619de2a8c224c295698d9af085af888d
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-08-26 17:57:59 +0300

    toggle menu bar button

---
+++ dist/js/all.js
@@ -0,0 +1,2 @@
+let menu=document.querySelector("#menu-btn"),navbar=document.querySelector(".header__navbar");menu.onclick=()=>{menu.classList.toggle("fa-times"),navbar.classList.toggle("active")},window.onscroll=()=>{menu.classList.remove("fa-times"),navbar.classList.remove("active")};
+//# sourceMappingURL=all.js.map
